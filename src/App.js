31d601d1ca5c20434f2d1aa6f89fd6717ea5ec13
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-06-10 17:32:12 +0900

    Added state to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      dishes: [],
+      currentDish: "",
+      img: ""
+    }
+  }
   render() {
+    return (
+      <div>Hello World</div>
+    );
   }
 }
 
