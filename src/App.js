d68b0e8781b2b0e0bfda777d3f4dd2c4e6283ca8
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-06-11 22:23:24 +0900

    Added form tags

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import SingleDish from './SingleDish';
 
 class App extends Component {
-  constructor (){
+  constructor() {
     super();
     this.state = {
       dishes: [],
       currentDish: "",
-      img: ""
+      img: "",
     }
   }
+  //This is for the input 
+  onInputChange = e => {
+    this.setState({ currentDish: e.target.value });
+  }
+  //This is for the button 
+  onClick = () => {
+    //Makes a copy of dishes list and keeps this pure
+    let dishesCopy = this.state.dishes.slice();
+    dishesCopy.push(this.state.currentDish);
+    this.setState({ dishes: dishesCopy, currentDish: "" });
+  }
   render() {
+    let listOfDishes = this.state.dishes.map((e, i) => {
+      return (
+       <SingleDish dish={e} key={i}/>
+      );
+    });
     return (
-      <div>Hello World</div>
+      <div>
+        <div>React Menu App</div>
+        <form >
+        <label>Dish:</label>
+        <input value={this.state.currentDish} onChange={this.onInputChange} />
+        <button onClick={this.onClick}>Add!</button>
+        <br/>
+        <ul>{listOfDishes}</ul>
+        </form>
+      </div>
+
     );
   }
 }
---
+++ src/SingleDish.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const SingleDish = props => {
+    return (
+        <li>
+            {props.dish}
+        </li>
+    );
+}
+
+export default SingleDish;
